15a3577497b1acac55ab4749081987b8316d648f
Ingyu Song <ingyu3879@gmail.com>
2023-04-20 14:37:40 +0900

    feat: ByteTypeExercise, ScannerFormatting 추가

---
+++ src/main/java/com/example/javaproject2/week1/ByteTypeExercise.java
@@ -0,0 +1,10 @@
+package com.example.javaproject2.week1;
+
+public class ByteTypeExercise {
+    public static void main(String[] args) {
+        byte byte1 = -128;
+        byte byte2 = 127;
+        System.out.println(byte1);
+        System.out.println(byte2);
+    }
+}
