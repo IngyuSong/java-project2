3d653369cf16d29796569b82651b9b7c4369c336
Ingyu Song <ingyu3879@gmail.com>
2023-04-20 15:19:01 +0900

    feat: VariableEx 추가

---
+++ src/main/java/com/example/javaproject2/week1/VariableEx.java
@@ -0,0 +1,17 @@
+package com.example.javaproject2.week1;
+
+import com.example.javaproject2.PrintHello;
+
+public class VariableEx {
+
+    public static void main(String[] args) {
+        int iVal;
+        iVal = 0;
+
+        PrintHello printHello;
+        printHello = new PrintHello();
+
+        System.out.println(iVal);
+        printHello.print();
+    }
+}
