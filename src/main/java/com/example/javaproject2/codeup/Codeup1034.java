ad64c4dd1551cc578b0513c320cdbcdfb261b9e4
Ingyu Song <ingyu3879@gmail.com>
2023-04-25 09:03:59 +0900

    feat: 2week-7day - CodeUp1031 ~ 1037

---
+++ src/main/java/com/example/javaproject2/codeup/Codeup1034.java
@@ -0,0 +1,10 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class Codeup1034 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(Integer.valueOf(sc.next(), 8));
+    }
+}
---
+++ src/main/java/com/example/javaproject2/codeup/Codeup1036.java
@@ -0,0 +1,11 @@
+package com.example.javaproject2.codeup;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Codeup1036 {
+    public static void main(String[] args) throws IOException {
+        InputStreamReader is = new InputStreamReader(System.in);
+        System.out.println(is.read());
+    }
+}
