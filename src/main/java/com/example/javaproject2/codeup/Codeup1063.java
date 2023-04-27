991ca0906e71acdb1b36cfa46e67f8e8169fa0f6
Ingyu Song <ingyu3879@gmail.com>
2023-04-27 15:31:23 +0900

    feat: 2week-4day - Codeup1063~1064 추가

---
+++ src/main/java/com/example/javaproject2/codeup/Codeup1063.java
@@ -0,0 +1,10 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class Codeup1063 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(Math.max(sc.nextInt(), sc.nextInt()));
+    }
+}
