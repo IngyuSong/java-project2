d656a849f172519ff733c2dcf4491c893b7ee125
Ingyu Song <ingyu3879@gmail.com>
2023-04-27 10:19:40 +0900

    feat: 2week-4day - IncreaseFirst 추가 - IncreaseFirst2 추가 - IncreaseOperator 추가

---
+++ src/main/java/com/example/javaproject2/week2/day4/IncreaseOperator.java
@@ -0,0 +1,9 @@
+package com.example.javaproject2.week2.day4;
+
+public class IncreaseOperator {
+    public static void main(String[] args) {
+        int i = 10;
+        i++;
+        System.out.println(i);
+    }
+}
