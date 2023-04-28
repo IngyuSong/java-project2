c3fcc317586f049adf41a9f59fc49ea0be461120
Ingyu Song <ingyu3879@gmail.com>
2023-04-28 11:23:27 +0900

    feat: 2week-5day - BubbleSort 추가

---
+++ src/main/java/com/example/javaproject2/week2/day5/BubbleSort.java
@@ -0,0 +1,26 @@
+package com.example.javaproject2.week2.day5;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++) {
+            array[i] = sc.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            int temp = 0;
+            for (int j = i; j < n - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(array));
+    }
+}
