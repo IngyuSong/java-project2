0a42b40b4e7626c3d1851e5272eb4e1449b3fc06
Ingyu Song <ingyu3879@gmail.com>
2023-05-01 14:48:24 +0900

    fix: MultiplicationTables 수정

--- src/main/java/com/example/javaproject2/week3/day1/MultiplicationTables.java
+++ src/main/java/com/example/javaproject2/week3/day1/MultiplicationTables.java
@@ -1,13 +1,20 @@
 package com.example.javaproject2.week3.day1;
 
+import java.util.Scanner;
+
 public class MultiplicationTables {
     public static void main(String[] args) {
-        for (int i = 2; i <= 4; i++) {
-            multiplication(i);
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.println("종료하려면 0을 입력하세요. ");
+            System.out.println("단 입력: ");
+            int dan = sc.nextInt();
+            if (dan == 0) return;
+            multiplication(dan);
         }
     }
 
-    public static void multiplication(int dan){
+    public static void multiplication(int dan) {
         System.out.printf("%d단\n", dan);
         for (int i = 1; i < 10; i++) {
             System.out.printf("%d * %d = %d\n", dan, i, dan * i);
