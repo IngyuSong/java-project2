b190ffbe0b9a0dc2803d6dad6c1381e438f65ef2
Ingyu Song <ingyu3879@gmail.com>
2023-05-02 15:24:24 +0900

    feat: 자릿수 합 구하기 추가

---
+++ src/main/java/com/example/javaproject2/programmers/DigitSum.java
@@ -0,0 +1,12 @@
+package com.example.javaproject2.programmers;
+
+public class DigitSum {
+    public int solution(int n) {
+        int answer = 0;
+        while (n > 0) {
+            answer += n % 10;
+            n /= 10;
+        }
+        return answer;
+    }
+}
