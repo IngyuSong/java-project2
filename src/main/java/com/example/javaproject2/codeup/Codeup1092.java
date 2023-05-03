1980c779c6de643cc85cf69b1c4069932e70b0e3
Ingyu Song <ingyu3879@gmail.com>
2023-05-03 15:44:52 +0900

    feat: Codeup1092~1095 추가

---
+++ src/main/java/com/example/javaproject2/codeup/Codeup1092.java
@@ -0,0 +1,22 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class Codeup1092 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(lcm(lcm(sc.nextInt(), sc.nextInt()), sc.nextInt()));
+    }
+
+    public static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        } else {
+            return gcd(b, a % b);
+        }
+    }
+
+    public static int lcm(int a, int b) {
+        return (a * b) / gcd(a, b);
+    }
+}
