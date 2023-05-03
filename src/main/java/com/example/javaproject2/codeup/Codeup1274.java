aec12088c852a000cbc20331211f0ac69ca1703c
Ingyu Song <ingyu3879@gmail.com>
2023-05-03 13:22:16 +0900

    feat: Codeup1274 추가

---
+++ src/main/java/com/example/javaproject2/codeup/Codeup1274.java
@@ -0,0 +1,15 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class Codeup1274 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        String isPrime = "prime";
+        for (int i = 2; i * i <= num; i++) {
+            if(num % i == 0) isPrime = "not prime";
+        }
+        System.out.println(isPrime);
+    }
+}
