f6fb83fbb7c84e05d929e1a6940a0742a412fd80
Ingyu Song <ingyu3879@gmail.com>
2023-05-08 11:16:43 +0900

    feat: SpaceInvaders, SpaceInvadersTest 추가

---
+++ src/main/java/com/example/javaproject2/week4/day1/SpaceInvaders.java
@@ -0,0 +1,12 @@
+package com.example.javaproject2.week4.day1;
+
+public class SpaceInvaders {
+    int location;
+    public void moveLeft() {
+        location--;
+    }
+
+    public void moveRight() {
+        location = location + 1;
+    }
+}
